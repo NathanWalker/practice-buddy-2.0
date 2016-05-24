ea5a68e8a49aa6717c50b2781a3bc7d5ed872df3
Nathan Walker <walkerrunpdx@gmail.com>
2016-05-23 18:53:06 -0700

    chore: cleanup

--- nativescript/platforms/ios/build/emulator/nativescript.app/app/tns_modules/nativescript-ng2-magic/src/client/plugin/services/magic.service.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class MagicService {
-    static ROUTER_DIRECTIVES: any;
-    static TEMPLATE_URL(path: string): string;
-    static IS_NATIVESCRIPT(): boolean;
-}
--- nativescript/platforms/ios/build/nativescript.build/Debug-iphonesimulator/nativescript.build/Objects-normal/i386/main.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/Looper/practice-buddy/nativescript/platforms/ios/internal/main.m \
-  /Users/Looper/practice-buddy/nativescript/platforms/ios/internal/NativeScript/include/NativeScript.h \
-  /Users/Looper/practice-buddy/nativescript/platforms/ios/internal/NativeScript/include/TNSRuntime.h \
-  /Users/Looper/practice-buddy/nativescript/platforms/ios/internal/NativeScript/include/TNSRuntime+Diagnostics.h \
-  /Users/Looper/practice-buddy/nativescript/platforms/ios/internal/NativeScript/include/TNSRuntime+Inspector.h \
-  /Users/Looper/practice-buddy/nativescript/platforms/ios/internal/TNSExceptionHandler.h \
-  /Users/Looper/practice-buddy/nativescript/platforms/ios/internal/TNSDebugging.h
